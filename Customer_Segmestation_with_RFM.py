31363407b84f7683c2ece971b658e87c8336ff3e
simalgezer <51541855+simalgezer@users.noreply.github.com>
2021-12-27 15:01:38 +0300

    Update Customer_Segmestation_with_RFM.py

--- Customer_Segmestation_with_RFM.py
+++ Customer_Segmestation_with_RFM.py
@@ -20,3 +20,42 @@ df = df[~df["Invoice"].str.contains("C", na=False)]
 df['TotalPrice']=df['Price']*df['Quantity']
 df.head()
 
+#Calculating RFM metrics
+df["InvoiceDate"].max()
+today_date = dt.datetime(2010, 12, 11)
+
+rfm = df.groupby('Customer ID').agg({'InvoiceDate': lambda date: (today_date - date.max()).days,
+                                     'Invoice': lambda num: num.nunique(),
+                                     'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+
+rfm.head()
+rfm.columns = ['recency', 'frequency', 'monetary']
+rfm.head()
+rfm.describe().T
+
+rfm = rfm[rfm["monetary"] > 0]
+
+#Assigning RFM Scores & mapping segments
+ rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+rfm["frequency_score"] = pd.qcut(rfm['frequency'].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
+rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) +
+                    rfm['frequency_score'].astype(str))
+
+rfm.describe().T
+rfm.head()
+
+seg_map = {
+    r'[1-2][1-2]': 'hibernating',
+    r'[1-2][3-4]': 'at_Risk',
+    r'[1-2]5': 'cant_loose',
+    r'3[1-2]': 'about_to_sleep',
+    r'33': 'need_attention',
+    r'[3-4][4-5]': 'loyal_customers',
+    r'41': 'promising',
+    r'51': 'new_customers',
+    r'[4-5][2-3]': 'potential_loyalists',
+    r'5[4-5]': 'champions'
+}
+rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
+rfm.head()
